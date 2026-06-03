3f3a9b51e7951d6213949b87711be1cb5826b3a2
Anh <anhdlcwk@gmail.com>
2026-06-04 00:59:29 +0700

    1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,46 @@
+import { Toaster, toast } from 'sonner';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router';
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFound';
 
 function App() {
+    return <>
+        <Toaster />
 
 
+
+        <button onClick={() => toast('Hello, world!')}> toast </button>
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+
+
+
+
+                <Route path="*" element={<NotFoundPage />} />
+
+
+
+
+
+
+            </Routes>
+
+
+
+
+
+
+
+
+        </BrowserRouter>
+
+
+
+
+    </>;
 
 }
 
-export default App
+export default App;
